669085a93e3dbe23fa69f5058a6ce847fb8f2cb3
Elton Chou <plscd748@gmail.com>
2021-09-25 02:25:29 +0000

    refined auth.webhook test

--- hook_web/libs/tasks.py
+++ hook_web/libs/tasks.py
@@ -1,5 +1,6 @@
 def send_notification_hook(webhook_id: str, webhook_token: str, msg: str):
-    pass
+    # TODO: prevent call celery function directly
+    raise NotImplementedError
     # send_new_hook_notification.apply_async(kwargs={
     #     'webhook_id': webhook_id,
     #     'webhook_token': webhook_token,
--- tests/test_routes/test_auth.py
+++ tests/test_routes/test_auth.py
@@ -2,50 +2,78 @@ from flask import url_for
 from pytest_mock import MockerFixture
 
 
-def test_webhook_auth(client, mocker: MockerFixture):
-    class MockAuthReponse:
-        status = 200
+class MockAuthReponse:
+    status = 200
 
-        @property
-        def status_code(self):
-            return self.status
+    @property
+    def status_code(self):
+        return self.status
 
-        def json(self):
-            return {
-                "webhook": {
-                    "channel_id": "123",
-                    "id": "564",
-                    "token": "secret"
-                }
+    def json(self):
+        return {
+            "webhook": {
+                "channel_id": "123",
+                "id": "564",
+                "token": "secret"
             }
+        }
 
-    from hook_web.libs.discord_api import DiscordApi
-    mocker.patch.object(DiscordApi, 'exchange_token',
-                        return_value=MockAuthReponse())
-    mocker.patch('hook_web.controllers.auth.save_webhook_info',
-                 return_value=True)
-    mocker.patch('hook_web.controllers.auth.check_state', return_value=True)
-    mocker.patch('hook_web.libs.tasks.send_notification_hook')
 
-    with client.session_transaction() as session:
-        session['webhook_setting'] = {'lang': 'zh-TW'}
-        session['entry_uri'] = '/'
+class FailedMockAuthReponse(MockAuthReponse):
+    status = 400
 
-    r = client.get(
-        url_for('auth.webhook'),
-        query_string={'code': "davidism", 'guild_id': "123", 'state': "123"},
-        follow_redirects=True
-    )
 
-    assert r.status_code == 200
-    assert b'message is-success' in r.data
+class TestWebhookAuth:
 
-    MockAuthReponse.status = 400
-    r = client.get(
-        url_for('auth.webhook'),
-        query_string={'code': "davidism", 'guild_id': "123", 'state': "123"},
-        follow_redirects=True
-    )
+    def test_successful_webhook_auth(self, client, mocker: MockerFixture):
+        from hook_web.libs.discord_api import DiscordApi
+        mocker.patch.object(DiscordApi, 'exchange_token',
+                            return_value=MockAuthReponse())
+        mocker.patch('hook_web.controllers.auth.save_webhook_info',
+                     return_value=True)
+        mocker.patch('hook_web.controllers.auth.check_state',
+                     return_value=True)
+        hook_sending = mocker.patch(
+            'hook_web.controllers.auth.send_notification_hook')
 
-    assert r.status_code == 200
-    assert b'message is-warning' in r.data
+        with client.session_transaction() as session:
+            session['webhook_setting'] = {'lang': 'zh-TW'}
+            session['entry_uri'] = '/'
+
+        r = client.get(
+            url_for('auth.webhook'),
+            query_string={'code': "davidism",
+                          'guild_id': "123", 'state': "123"},
+            follow_redirects=True
+        )
+
+        assert r.status_code == 200
+        assert b'message is-success' in r.data
+        assert hook_sending.call_count == 1
+
+        def test_faile_webhook_auth(self, client, mocker: MockerFixture):
+            from hook_web.libs.discord_api import DiscordApi
+            mocker.patch.object(DiscordApi, 'exchange_token',
+                                return_value=FailedMockAuthReponse())
+            saving_webhook = mocker.patch('hook_web.controllers.auth.save_webhook_info',
+                                          return_value=True)
+            mocker.patch('hook_web.controllers.auth.check_state',
+                         return_value=True)
+            hook_sending = mocker.patch(
+                'hook_web.controllers.auth.send_notification_hook')
+
+            with client.session_transaction() as session:
+                session['webhook_setting'] = {'lang': 'zh-TW'}
+                session['entry_uri'] = '/'
+
+            r = client.get(
+                url_for('auth.webhook'),
+                query_string={'code': "davidism",
+                              'guild_id': "123", 'state': "123"},
+                follow_redirects=True
+            )
+
+            assert r.status_code == 400
+            assert b'message is-warning' in r.data
+            assert not hook_sending.called
+            assert not saving_webhook.called
