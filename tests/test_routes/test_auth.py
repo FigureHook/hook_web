5f40682be758eaba1914d111f37d5f8147e4d389
Elton Chou <plscd748@gmail.com>
2021-09-25 03:16:42 +0000

    fixed indent

--- tests/test_routes/test_auth.py
+++ tests/test_routes/test_auth.py
@@ -51,29 +51,29 @@ class TestWebhookAuth:
         assert b'message is-success' in r.data
         assert hook_sending.call_count == 1
 
-        def test_faile_webhook_auth(self, client, mocker: MockerFixture):
-            from hook_web.libs.discord_api import DiscordApi
-            mocker.patch.object(DiscordApi, 'exchange_token',
-                                return_value=FailedMockAuthReponse())
-            saving_webhook = mocker.patch('hook_web.controllers.auth.save_webhook_info',
-                                          return_value=True)
-            mocker.patch('hook_web.controllers.auth.check_state',
-                         return_value=True)
-            hook_sending = mocker.patch(
-                'hook_web.controllers.auth.send_notification_hook')
+    def test_faile_webhook_auth(self, client, mocker: MockerFixture):
+        from hook_web.libs.discord_api import DiscordApi
+        mocker.patch.object(DiscordApi, 'exchange_token',
+                            return_value=FailedMockAuthReponse())
+        saving_webhook = mocker.patch('hook_web.controllers.auth.save_webhook_info',
+                                      return_value=True)
+        mocker.patch('hook_web.controllers.auth.check_state',
+                     return_value=True)
+        hook_sending = mocker.patch(
+            'hook_web.controllers.auth.send_notification_hook')
 
-            with client.session_transaction() as session:
-                session['webhook_setting'] = {'lang': 'zh-TW'}
-                session['entry_uri'] = '/'
+        with client.session_transaction() as session:
+            session['webhook_setting'] = {'lang': 'zh-TW'}
+            session['entry_uri'] = '/'
 
-            r = client.get(
-                url_for('auth.webhook'),
-                query_string={'code': "davidism",
-                              'guild_id': "123", 'state': "123"},
-                follow_redirects=True
-            )
+        r = client.get(
+            url_for('auth.webhook'),
+            query_string={'code': "davidism",
+                          'guild_id': "123", 'state': "123"},
+            follow_redirects=True
+        )
 
-            assert r.status_code == 400
-            assert b'message is-warning' in r.data
-            assert not hook_sending.called
-            assert not saving_webhook.called
+        assert r.status_code == 200
+        assert b'message is-warning' in r.data
+        assert not hook_sending.called
+        assert not saving_webhook.called
