034b801eb28c99f0a90ad79db428b4f6028cd5e3
Elton Chou <plscd748@gmail.com>
2021-11-13 13:57:33 +0000

    add: implement notification webhook sending

--- hook_web/libs/tasks.py
+++ hook_web/libs/tasks.py
@@ -1,8 +1,9 @@
+from hook_tasks.on_demand.tasks import send_discord_webhook
+
+
 def send_notification_hook(webhook_id: str, webhook_token: str, msg: str):
-    # TODO: prevent call celery function directly
-    raise NotImplementedError
-    # send_new_hook_notification.apply_async(kwargs={
-    #     'webhook_id': webhook_id,
-    #     'webhook_token': webhook_token,
-    #     'msg': msg
-    # })
+    send_discord_webhook.apply_async(kwargs={
+        'webhook_id': webhook_id,
+        'webhook_token': webhook_token,
+        'msg': msg
+    })
