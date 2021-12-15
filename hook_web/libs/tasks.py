da635c514016b88a8ea4069773bd5557be90228e
Elton Chou <plscd748@gmail.com>
2021-12-15 15:28:16 +0000

    hotfix: temporary fix for send webhook

--- hook_web/libs/tasks.py
+++ hook_web/libs/tasks.py
@@ -1,9 +1,10 @@
 from hook_tasks.on_demand.tasks import send_discord_webhook
 
 
-def send_notification_hook(webhook_id: str, webhook_token: str, msg: str):
-    send_discord_webhook.apply_async(kwargs={
-        'webhook_id': webhook_id,
-        'webhook_token': webhook_token,
-        'msg': msg
-    })
+def send_notification_hook(webhook_id: int, webhook_token: str, msg: str):
+    send_discord_webhook(webhook_id, webhook_token, msg)
+    # send_discord_webhook.apply_async(kwargs={
+    #     'webhook_id': webhook_id,
+    #     'webhook_token': webhook_token,
+    #     'msg': msg
+    # })
