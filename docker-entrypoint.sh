2ee31c6d1e14bdd4568ef1f6157c4d0314102634
Elton Chou <plscd748@gmail.com>
2021-12-15 18:31:28 +0000

    add: add docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+arg1=$1
+
+check_db() {
+    if ! hookweb check db; then
+        echo "Can't get connection with database."
+    exit 1
+    fi
+}
+
+check_redis() {
+    if ! hookweb check db; then
+        echo "Can't get connection with redis."
+    exit 1
+    fi
+}
+
+if [ -n $WORKERS ]; then
+    WORKERS=2
+fi
+
+if [ -n $PORT ]; then
+    PORT=8000
+fi
+
+if [ $arg1 == "start" ]; then
+    check_db
+    check_redis
+    gunicorn -w ${WORKERS} -b 0.0.0.0:${PORT} hook_web.wsgi:app
+fi
+
+if [ $arg1 == "test" ]; then
+    pytest
+fi
